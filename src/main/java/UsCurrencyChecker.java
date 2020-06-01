af8f97b1d5d5750b48ca610868f6fd2adfdeee12
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-06-01 18:44:36 +0200

    escape dot char

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        String regex = "\\$\\s*\\d*(.\\d\\d?)?";
+        String regex = "\\$ ?\\d*((\\.)\\d\\d?)?";
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(usCurrencyString);
     }
