b8cb6b6b11296bedd995c20cf847622d7208e41f
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-06-01 18:27:36 +0200

    setup

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,10 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        String regex = "";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(usCurrencyString);
     }
 }
