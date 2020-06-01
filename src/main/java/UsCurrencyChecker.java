7c0b1521cb53a95047a8b420f067aee36b1dd59c
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-06-01 18:41:21 +0200

    first attempt

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        String regex = "";
+        String regex = "\\$\\s*\\d*(.\\d\\d?)?";
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(usCurrencyString);
     }
